8bb42b69b85d1c2819114d2ae52249b2f00ca4f7
Sindhu Yamsani <92909904+sindhuyamsani@users.noreply.github.com>
2022-06-25 00:22:21 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,19 @@
+test=int(input())
+
+while(test!=0):
+    
+        
+    n=int(input())
+    w=list(map(int,input().split()))
+    w=w.sort()
+    print(w)
+    p=0
+
+    for i in range(n):
+        p=p+w[i]
+        w.pop(0)
+        for j in range(len(w)):
+            if w[j]>0:
+                w[j]=w[j]-1
+    print(p)
+            
